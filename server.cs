67406a762fba20fa131bad3f5fd863181d843674
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-06-30 15:53:33 -0400

    Make this not suck, return public key, and actually sign data

--- server.cs
+++ server.cs
@@ -3,99 +3,223 @@ using System.IO;
 using System.Text;
 using System.Net;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Collections.Generic;
 using PKHeX.Core;
+using dotenv.net;
 
 namespace PKHaX {
 	class Server {
 		public static HttpListener listener;
-		
-		public static string protocol = "http";
-		public static int port = 80;
 
-		public static byte[] ILLEGAL_POKEMON_MAGIC = new byte[] { 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x0A };
+		public static string protocol = "http";
+		public static int port = 9000;
+		public static byte[] EXPECTED_CERTIFICATE_ID = new byte[] { 0xFF, 0xFF };
+		public static byte[] INVALID_CERTIFICATE_ID_RESPONSE = new byte[] { 0x02 };
+		public static byte[] ILLEGAL_POKEMON_RESPONSE = new byte[] { 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x0A };
 		public static byte[] LEGAL_POKEMON_MAGIC = new byte[] { 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00 };
-		public static byte[] POKEMON_SIG = new byte[] {
-			0x7E, 0x1B, 0xE6, 0x53, 0x6A, 0xB5, 0x51, 0xD8, 0x9C, 0xB4, 0x61, 0xA7,
-			0x9D, 0xDE, 0x57, 0x06, 0x5C, 0x57, 0x41, 0x73, 0x6C, 0x0D, 0x4C, 0x24,
-			0x52, 0xAD, 0x09, 0x34, 0xE7, 0xC8, 0xD2, 0x88, 0x76, 0xCC, 0x0C, 0x74,
-			0x79, 0x27, 0x59, 0x1B, 0xC2, 0x84, 0x46, 0x26, 0x2E, 0x43, 0xA9, 0xCF,
-			0x54, 0xDA, 0xDD, 0x82, 0x65, 0x27, 0xAA, 0xFD, 0xAF, 0x37, 0x4C, 0xCE,
-			0x8A, 0xAC, 0x13, 0x41, 0x3C, 0x5C, 0x32, 0xFB, 0xDA, 0x4D, 0xDF, 0xB9,
-			0x46, 0xC8, 0x77, 0x52, 0x26, 0x03, 0x6C, 0xA3, 0x6A, 0xC8, 0xA7, 0xA8,
-			0xCD, 0x6B, 0x17, 0xF6, 0x60, 0x93, 0xFD, 0x32, 0x48, 0x54, 0x1E, 0x8F,
-			0x44, 0xCB, 0x9B, 0x51, 0xFA, 0x1F, 0xBD, 0x5E, 0xF2, 0xBD, 0x73, 0xD2,
-			0xDA, 0xAE, 0x9E, 0x1D, 0x37, 0x63, 0xD3, 0xF7, 0xDF, 0xF8, 0xF0, 0xEB,
-			0x52, 0x2F, 0x6A, 0x76, 0x2A, 0x13, 0xEB, 0xAF, 0x31, 0x05, 0x58, 0x9E,
-			0xDC, 0x95, 0xC4, 0x69, 0x53, 0x34, 0xBC, 0x2C, 0xEB, 0x63, 0x2C, 0xAE,
-			0x97, 0xA2, 0xCA, 0xDC, 0x3C, 0x6D, 0x57, 0xFA, 0x9C, 0xAC, 0xF3, 0xA9,
-			0x67, 0x4E, 0x16, 0x53, 0xA9, 0xFA, 0x95, 0xF0, 0x55, 0xB4, 0x70, 0x42,
-			0x1E, 0x1F, 0x65, 0xD6, 0xF6, 0x3D, 0x69, 0x6B, 0xF7, 0xCD, 0x45, 0xBC,
-			0x69, 0xE0, 0xB9, 0x14, 0x8A, 0xC9, 0x35, 0x56, 0x4B, 0xE8, 0x3D, 0x29,
-			0x9A, 0xF5, 0xCB, 0xEB, 0x2A, 0x14, 0x8D, 0x96, 0x1C, 0x42, 0x09, 0x7D,
-			0xED, 0xEB, 0xA7, 0x00, 0xEE, 0xA4, 0x0D, 0x65, 0x26, 0x3C, 0xDE, 0x47,
-			0x83, 0x64, 0x90, 0xE1, 0xA7, 0x11, 0x4C, 0xDA, 0x12, 0x64, 0x97, 0xA8,
-			0xED, 0x9F, 0xC8, 0xFE, 0x12, 0x65, 0x78, 0xB2, 0x60, 0x86, 0xE0, 0x02,
-			0x28, 0xC4, 0x5A, 0x17, 0x7B, 0xE4, 0xBF, 0xA3, 0x10, 0x51, 0x18, 0x9E,
-			0x9A, 0xEB, 0xEC, 0x63
-		};
+		public static RSA RSA_KEY_PAIR = RSA.Create();
+
+		public static Dictionary<string, Dictionary<string, Func<HttpListenerRequest, byte[]>>> REQUEST_HANDLERS = new Dictionary<string, Dictionary<string, Func<HttpListenerRequest, byte[]>>>();
 
 		public static async Task HandleIncomingConnections() {
 			while (true) {
 				HttpListenerContext ctx = await listener.GetContextAsync();
 
-				HttpListenerRequest req = ctx.Request;
-				HttpListenerResponse resp = ctx.Response;
+				HttpListenerRequest request = ctx.Request;
+				HttpListenerResponse response = ctx.Response;
 
-				resp.StatusCode = 404;
+				response.StatusCode = 404;
 
-				if ((req.HttpMethod == "POST") && (req.Url.AbsolutePath == "/validator/v1/validate")) {
-					MemoryStream ms = new MemoryStream();
-					req.InputStream.CopyTo(ms);
+				if (REQUEST_HANDLERS.ContainsKey(request.HttpMethod)) {
+					Dictionary<string, Func<HttpListenerRequest, byte[]>> methodHandlers = REQUEST_HANDLERS[request.HttpMethod];
 
-					byte[] body = ms.ToArray();
+					if (methodHandlers.ContainsKey(request.Url.AbsolutePath)) {
+						Func<HttpListenerRequest, byte[]> handler = methodHandlers[request.Url.AbsolutePath];
+						byte [] responseData = handler(request);
 
-					byte[] serviceToken = new byte[0x58];
-					byte[] unknown = new byte[0xA7];
-					byte[] encryptedPkm = new byte[0xE8];
+						response.ContentLength64 = responseData.LongLength;
+						response.StatusCode = 200;
 
-					Array.Copy(body, 0, serviceToken, 0, 0x58);
-					Array.Copy(body, 0x58, unknown, 0, 0xA7);
-					Array.Copy(body, 0x58 + 0xA7, encryptedPkm, 0, 0xE8);
-
-					var pkm = new PK6(encryptedPkm);
-					var la = new LegalityAnalysis(pkm);
-
-					byte[] responseData;
-
-					if (la.Valid) {
-						responseData = new byte[0x107];
-						Array.Copy(LEGAL_POKEMON_MAGIC, 0, responseData, 0, LEGAL_POKEMON_MAGIC.Length);
-						Array.Copy(POKEMON_SIG, 0, responseData, LEGAL_POKEMON_MAGIC.Length, POKEMON_SIG.Length);
-
-						// Real server sends back an RSA-256 signature here
-						// after the magic
-
-						// We do not have the private key, and the public key
-						// is requested one time when making a new save and
-						// stored in the save file, making it risky to patch
-
-						// Leaving signature as all null bytes for now
-					} else {
-						responseData = new byte[0x07];
-						Array.Copy(ILLEGAL_POKEMON_MAGIC, 0, responseData, 0, ILLEGAL_POKEMON_MAGIC.Length);
+						await response.OutputStream.WriteAsync(responseData, 0, responseData.Length);
 					}
-
-					resp.ContentLength64 = responseData.LongLength;
-					resp.StatusCode = 200;
-
-					await resp.OutputStream.WriteAsync(responseData, 0, responseData.Length);
 				}
 
-				resp.Close();
+				response.Close();
+			}
+		}
+
+		public static byte[] ValidatorV1Validate(HttpListenerRequest req) {
+			MemoryStream ms = new MemoryStream();
+			req.InputStream.CopyTo(ms);
+
+			byte[] body = ms.ToArray();
+
+			byte[] serviceToken = new byte[0x58];
+			byte[] unknown = new byte[0xA7];
+			byte[] encryptedPokemon = new byte[0xE8];
+
+			int serviceTokenOffset = 0;
+			int unknownOffset = serviceTokenOffset + serviceToken.Length;
+			int encryptedPokemonOffset = unknownOffset + unknown.Length;
+
+			Array.Copy(body, serviceTokenOffset, serviceToken, 0, serviceToken.Length);
+			Array.Copy(body, unknownOffset, unknown, 0, unknown.Length);
+			Array.Copy(body, encryptedPokemonOffset, encryptedPokemon, 0, encryptedPokemon.Length);
+
+			// TODO - VERIFY SERVICE TOKEN
+
+			byte[] certificateID = new byte[0x2];
+
+			Array.Copy(unknown, 0, certificateID, 0, certificateID.Length);
+
+			if (!certificateID.SequenceEqual(EXPECTED_CERTIFICATE_ID)) {
+				return INVALID_CERTIFICATE_ID_RESPONSE;
+			}
+
+			PK6 pokemon = new PK6(encryptedPokemon);
+			LegalityAnalysis legalityAnalysis = new LegalityAnalysis(pokemon);
+
+			if (!legalityAnalysis.Valid) {
+				return ILLEGAL_POKEMON_RESPONSE;
+			}
+
+			HashAlgorithmName algorithm = HashAlgorithmName.SHA256;
+			RSASignaturePadding padding = RSASignaturePadding.Pkcs1;
+
+			// * WE DON'T ACTUALLY KNOW WHAT DATA THIS SIGNATURE IS OVER!
+			// * LEAVING IT LIKE THIS FOR NOW UNTIL WE FIND IT
+			byte[] signature = RSA_KEY_PAIR.SignData(encryptedPokemon, 0, encryptedPokemon.Length, algorithm, padding);
+			byte[] responseData = new byte[LEGAL_POKEMON_MAGIC.Length + signature.Length];
+
+			Array.Copy(LEGAL_POKEMON_MAGIC, 0, responseData, 0, LEGAL_POKEMON_MAGIC.Length);
+			Array.Copy(signature, 0, responseData, LEGAL_POKEMON_MAGIC.Length, signature.Length);
+
+			return responseData;
+		}
+
+		public static byte[] ValidatorV1PublicKey(HttpListenerRequest req) {
+			// TODO - VERIFY SERVICE TOKEN
+
+			// TODO - Is there a better way to do this? I'm new to c# 💀
+			byte[] publicKeyBytes = RSA_KEY_PAIR.ExportRSAPublicKey();
+			string publicKeyBase64String = System.Convert.ToBase64String(publicKeyBytes);
+			byte[] publicKeyBase64Bytes = Encoding.ASCII.GetBytes(publicKeyBase64String);
+
+			byte[] responseData = new byte[EXPECTED_CERTIFICATE_ID.Length + publicKeyBase64Bytes.Length];
+
+			Array.Copy(EXPECTED_CERTIFICATE_ID, 0, responseData, 0, EXPECTED_CERTIFICATE_ID.Length);
+			Array.Copy(publicKeyBase64Bytes, 0, responseData, EXPECTED_CERTIFICATE_ID.Length, publicKeyBase64Bytes.Length);
+
+			return responseData;
+		}
+
+		public static void ImportRSAKey() {
+			string privateKeyPath = System.Environment.GetEnvironmentVariable("PKHAX_PRIVATE_KEY_PATH");
+
+			if (String.IsNullOrEmpty(privateKeyPath)) {
+				Console.WriteLine("PKHAX_PRIVATE_KEY_PATH is not set. Set PKHAX_PRIVATE_KEY_PATH to the path of your RSA 2048 private key PEM");
+				System.Environment.Exit(1);
+			}
+
+			if (!File.Exists(privateKeyPath)) {
+				Console.WriteLine("File {0} does not exist. Set PKHAX_PRIVATE_KEY_PATH to the path of your RSA 2048 private key PEM", privateKeyPath);
+				System.Environment.Exit(1);
+			}
+
+			try {
+				string privateKeyText = File.ReadAllText(privateKeyPath);
+
+				RSA_KEY_PAIR.ImportFromPem(privateKeyText);
+			} catch (System.Exception) {
+				Console.WriteLine("Invalid RSA private key PEM");
+				throw;
+			}
+
+			// * RSA keys can only be 2048
+			if (RSA_KEY_PAIR.KeySize != 2048) {
+				Console.WriteLine("Invalid RSA key size. Expected 2048, got {0}", RSA_KEY_PAIR.KeySize);
+				System.Environment.Exit(1);
+			}
+
+			// * Dirty check to see if the key pair really contains a private key
+			// TODO - Better way to do this?
+			try {
+				RSA_KEY_PAIR.ExportRSAPrivateKey();
+			} catch (System.Exception) {
+				Console.WriteLine("RSA key provided is not a private key. Please provide an RSA 2048 private key");
+				throw;
+			}
+		}
+
+		public static void CreateRequestHandlers() {
+			Dictionary<string, Func<HttpListenerRequest, byte[]>> POSTHandlers = new Dictionary<string, Func<HttpListenerRequest, byte[]>>();
+
+			POSTHandlers.Add("/validator/v1/validate", ValidatorV1Validate);
+			POSTHandlers.Add("/validator/v1/public_key", ValidatorV1PublicKey);
+
+			REQUEST_HANDLERS.Add("POST", POSTHandlers);
+		}
+
+		public static void CheckEnvironmentVariables() {
+			CheckPortEnvironmentVariable();
+			CheckCertificateIDEnvironmentVariable();
+		}
+
+		public static void CheckPortEnvironmentVariable() {
+			string customPortString = System.Environment.GetEnvironmentVariable("PKHAX_PORT");
+
+			if (!String.IsNullOrEmpty(customPortString)) {
+				if (Int32.TryParse(customPortString, out int customPort)) {
+					port = customPort;
+				} else {
+					Console.WriteLine("{0} is not a valid number. Using default port {1}", customPortString, port);
+				}
+			} else {
+				Console.WriteLine("No port set. Using default port {0}", port);
+			}
+		}
+
+		public static void CheckCertificateIDEnvironmentVariable() {
+			string customCertificateIDString = System.Environment.GetEnvironmentVariable("PKHAX_CERTIFICATE_ID");
+
+			if (!String.IsNullOrEmpty(customCertificateIDString)) {
+				int hexLength = customCertificateIDString.Length;
+
+				if (hexLength != 4) {
+					Console.WriteLine("Invalid certificate ID. Certificate IDs must be set as exactly 2 bytes represented as 4 hex characters (ex, ABCD), got {0}", customCertificateIDString);
+					System.Environment.Exit(1);
+				}
+
+				byte[] bytes = new byte[hexLength / 2];
+				for (int i = 0; i < hexLength; i += 2) {
+					string chunk = customCertificateIDString.Substring(i, 2);
+
+					try {
+						bytes[i / 2] = Convert.ToByte(chunk, 16);
+					} catch (System.Exception) {
+						Console.WriteLine("Invalid certificate ID. Chunk {0} is not valid hex. Certificate IDs must be set as exactly 2 bytes represented as 4 hex characters (ex, ABCD)", chunk);
+						System.Environment.Exit(1);
+					}
+				}
+
+				EXPECTED_CERTIFICATE_ID = bytes;
+
+				Console.WriteLine("Using certificate ID {0}", Convert.ToHexString(bytes));
+			} else {
+				Console.WriteLine("No certificate ID set. Using default ID {0}", Convert.ToHexString(EXPECTED_CERTIFICATE_ID));
 			}
 		}
 
 		public static void Main(string[] args) {
+			DotEnv.Load();
+
+			ImportRSAKey();
+			CreateRequestHandlers();
+			CheckEnvironmentVariables();
+
+			// TODO - It would be nice to display the current PKHeX version at startup
+
 			string prefix = string.Format("{0}://*:{1}/", protocol, port);
 
 			listener = new HttpListener();
