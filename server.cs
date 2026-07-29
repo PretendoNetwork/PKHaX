5bca3d683dd733d6b2a37849479db6009ec1b9b9
Jonathan Barrow <jonbarrow1998@gmail.com>
2026-07-28 22:13:46 -0400

    feat: move /validator/v1/validate to use classes and handle both request types
    a lot of context is still missing, but this sets the bones in place

--- server.cs
+++ server.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography;
 using PKHeX.Core;
 using dotenv.net;
 using System.Buffers;
+using System.Buffers.Binary;
 using System.Text.Json;
 
 namespace PKHaX {
@@ -12,7 +13,7 @@ namespace PKHaX {
 
 		public static string protocol = "http";
 		public static int port = 9000;
-		public static byte[] EXPECTED_CERTIFICATE_ID = new byte[] { 0x03, 0x00 };
+		public static ushort EXPECTED_CERTIFICATE_ID = 3;
 		public static byte[] INVALID_CERTIFICATE_ID_RESPONSE = new byte[] { 0x02 };
 		public static byte[] ILLEGAL_POKEMON_RESPONSE = new byte[] { 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x0A };
 		public static byte[] LEGAL_POKEMON_MAGIC = new byte[] { 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00 };
@@ -20,6 +21,36 @@ namespace PKHaX {
 			0x30, 0x82, 0x01, 0x22, 0x30, 0x0D, 0x06, 0x09, 0x2A, 0x86, 0x48, 0x86,
 			0xF7, 0x0D, 0x01, 0x01, 0x01, 0x05, 0x00, 0x03, 0x82, 0x01, 0x0F, 0x00
 		};
+
+		// TODO - Give these values proper names when more context is found
+		public enum ValidatorV1ValidatePayloadType : ushort {
+			Type1 = 1, // * Seems to be nearly identical to Type2, but has an extra 0xA0 blob before the Pokemon data?
+			Type2 = 2
+		}
+
+		public sealed class ValidatorV1ValidateRequest {
+			public string ServiceToken { get; init; } = "";
+			public ushort CertificateID { get; init; }
+			public ValidatorV1ValidatePayloadType PayloadType { get; init; }
+			public object Payload { get; init; } = null!;
+		}
+
+		// TODO - Give these structs proper names when more context is found
+		public sealed class ValidatorV1ValidateType1Payload {
+			public ushort Count { get; init; }
+			public List<ValidatorV1ValidateType1Entry> Entries { get; init; } = new();
+		}
+
+		public sealed class ValidatorV1ValidateType1Entry {
+			// TODO - Can PKHeX handle this "Unknown" section natively or no? Should we even bother separating this out, what is this, have we just been getting lucky this whole time?
+			public byte[] Unknown { get; init; } = null!;
+			public byte[] EncryptedPokemon { get; init; } = null!;
+		}
+
+		public sealed class ValidatorV1ValidateType2Payload {
+			public ushort Count { get; init; }
+			public List<byte[]> EncryptedPokemon { get; init; } = new();
+		}
 
 		public static RSA RSA_KEY_PAIR = RSA.Create();
 
@@ -60,53 +91,147 @@ namespace PKHaX {
 			}
 		}
 
-		// Looks like this endpoint is used for multiple actions. The current implementation only handles one of the variants.
-		// TODO implement the other variants of this endpoint.
+		public static ValidatorV1ValidateRequest ParseValidatorV1ValidateRequest(ReadOnlyMemory<byte> body) {
+			var reader = new SequenceReader<byte>(new ReadOnlySequence<byte>(body));
+
+			if (!reader.TryReadTo(out ReadOnlySequence<byte> serviceTokenBytes, 0)) {
+				throw new InvalidDataException("Failed to read service token");
+			}
+
+			string serviceToken = Encoding.ASCII.GetString(serviceTokenBytes.ToArray());
+
+			if (!reader.TryReadLittleEndian(out short certificateID)) {
+				throw new InvalidDataException("Failed to read certificate ID");
+			}
+
+			if (!reader.TryReadBigEndian(out short payloadType)) {
+				throw new InvalidDataException("Failed to read payload type");
+			}
+
+			object payload = (ValidatorV1ValidatePayloadType)(ushort)payloadType switch{
+				ValidatorV1ValidatePayloadType.Type1 => ParseValidatorV1ValidateType1Payload(ref reader),
+				ValidatorV1ValidatePayloadType.Type2 => ParseValidatorV1ValidateType2Payload(ref reader),
+				_ => throw new InvalidDataException($"Unknown payload type {payloadType}")
+			};
+
+			if (reader.Remaining != 0) {
+				Console.WriteLine($"WARN: Payload type {payloadType} has {reader.Remaining} remaining bytes unparsed");
+			}
+
+			return new ValidatorV1ValidateRequest {
+				ServiceToken = serviceToken,
+				CertificateID = (ushort)certificateID,
+				PayloadType = (ValidatorV1ValidatePayloadType)(ushort)payloadType,
+				Payload = payload
+			};
+		}
+
+		public static ValidatorV1ValidateType1Payload ParseValidatorV1ValidateType1Payload(ref SequenceReader<byte> reader) {
+			if (!reader.TryReadBigEndian(out short count)) {
+				throw new InvalidDataException("Failed to read count");
+			}
+
+			var payload = new ValidatorV1ValidateType1Payload {
+				Count = (ushort)count
+			};
+
+			for (int i = 0; i < payload.Count; i++) {
+				if (!reader.TryReadExact(0xA0, out var unknown)) {
+					throw new InvalidDataException("Failed to read unknown data");
+				}
+
+				if (!reader.TryReadExact(0xE8, out var pokemon)) {
+					throw new InvalidDataException("Failed to read encrypted Pokemon data");
+				}
+
+				payload.Entries.Add(new ValidatorV1ValidateType1Entry {
+					Unknown = unknown.ToArray(),
+					EncryptedPokemon = pokemon.ToArray()
+				});
+			}
+
+			return payload;
+		}
+
+		public static ValidatorV1ValidateType2Payload ParseValidatorV1ValidateType2Payload(ref SequenceReader<byte> reader) {
+			if (!reader.TryReadBigEndian(out short count)) {
+				throw new InvalidDataException("Failed to read count");
+			}
+
+			var payload = new ValidatorV1ValidateType2Payload {
+				Count = (ushort)count
+			};
+
+			for (int i = 0; i < payload.Count; i++) {
+				if (!reader.TryReadExact(0xE8, out var pokemon)) {
+					throw new InvalidDataException("Failed to read encrypted Pokemon data");
+				}
+
+				payload.EncryptedPokemon.Add(pokemon.ToArray());
+			}
+
+			return payload;
+		}
+
+		// * Handles validating Pokemon data for legality
 		public static byte[] ValidatorV1Validate(HttpListenerRequest req) {
 			using var ms = new MemoryStream();
 			req.InputStream.CopyTo(ms);
-			byte[] body = ms.ToArray();
 
-			var sequence = new ReadOnlySequence<byte>(body);
-			var reader = new SequenceReader<byte>(sequence);
-
-			reader.TryReadTo(out ReadOnlySequence<byte> serviceToken, 0x00); // Read to (and advance past) NULL byte
-			reader.TryReadExact(0x6, out var requestInfo);
-			reader.TryReadExact(0xA0 + 0xE8, out var encryptedPokemonAndPadding);
-			if (reader.Remaining > 0) throw new Exception($"Expected EOF but got {reader.Remaining} remaining bytes");
-			var encryptedPokemon = encryptedPokemonAndPadding.Slice(0xA0); // Slice off the padding
+			var body = ms.GetBuffer().AsMemory(0, (int)ms.Length);
+			ValidatorV1ValidateRequest request = ParseValidatorV1ValidateRequest(body);
 
 			// TODO - VERIFY SERVICE TOKEN
 
-			var certificateID = requestInfo.Slice(0, 0x2).ToArray();
-
-			if (!certificateID.SequenceEqual(EXPECTED_CERTIFICATE_ID)) {
-				Console.WriteLine("WARN: Invalid certificate ID");
+			if (request.CertificateID != EXPECTED_CERTIFICATE_ID) {
+				// * This is expected to happen, since the game requests the certificate at the start of a save file
+				// * and then never again unless the server tells it to. So people with existing saves will not have
+				// * our certificate ID
 				return INVALID_CERTIFICATE_ID_RESPONSE;
 			}
 
-			PK6 pokemon = new PK6(encryptedPokemon.ToArray());
-			LegalityAnalysis legalityAnalysis = new LegalityAnalysis(pokemon);
+			switch (request.Payload) {
+				case ValidatorV1ValidateType1Payload payload:
+					foreach (var entry in payload.Entries) {
+						var encryptedPokemon = entry.EncryptedPokemon;
+						PK6 pokemon = new PK6(encryptedPokemon.ToArray());
+						LegalityAnalysis legalityAnalysis = new LegalityAnalysis(pokemon);
 
-			if (!legalityAnalysis.Valid)
-			{
-				if (!legalityAnalysis.Parsed)
-				{
-					Console.WriteLine($"WARN: Invalid pokemon: Failed to parse");
-				}
-				else
-				{
-					Console.WriteLine($"WARN: Invalid pokemon: {JsonSerializer.Serialize(legalityAnalysis.Results)}");
-				}
-				return ILLEGAL_POKEMON_RESPONSE;
+						if (!legalityAnalysis.Valid) {
+							if (!legalityAnalysis.Parsed) {
+								Console.WriteLine($"WARN: Invalid pokemon: Failed to parse");
+							} else {
+								Console.WriteLine($"WARN: Invalid pokemon: {JsonSerializer.Serialize(legalityAnalysis.Results)}");
+							}
+
+							return ILLEGAL_POKEMON_RESPONSE;
+						}
+					}
+					break;
+				case ValidatorV1ValidateType2Payload payload:
+					foreach (var encryptedPokemon in payload.EncryptedPokemon) {
+						PK6 pokemon = new PK6(encryptedPokemon.ToArray());
+						LegalityAnalysis legalityAnalysis = new LegalityAnalysis(pokemon);
+
+						if (!legalityAnalysis.Valid) {
+							if (!legalityAnalysis.Parsed) {
+								Console.WriteLine($"WARN: Invalid pokemon: Failed to parse");
+							} else {
+								Console.WriteLine($"WARN: Invalid pokemon: {JsonSerializer.Serialize(legalityAnalysis.Results)}");
+							}
+
+							return ILLEGAL_POKEMON_RESPONSE;
+						}
+					}
+					break;
 			}
 
 			HashAlgorithmName algorithm = HashAlgorithmName.SHA256;
 			RSASignaturePadding padding = RSASignaturePadding.Pkcs1;
 
-			// * WE DON'T ACTUALLY KNOW WHAT DATA THIS SIGNATURE IS OVER!
-			// * LEAVING IT LIKE THIS FOR NOW UNTIL WE FIND IT
-			byte[] signature = RSA_KEY_PAIR.SignData(encryptedPokemonAndPadding.ToArray(), algorithm, padding);
+			// TODO - WE DON'T ACTUALLY KNOW WHAT DATA THIS SIGNATURE IS OVER! LEAVING IT LIKE THIS FOR NOW UNTIL WE FIND IT
+			// * We have sigpatches for these signatures so this doesn't super matter
+			byte[] signature = RSA_KEY_PAIR.SignData(body.ToArray(), algorithm, padding);
 			byte[] responseData = LEGAL_POKEMON_MAGIC.Concat(signature).ToArray();
 
 			return responseData;
@@ -115,16 +240,17 @@ namespace PKHaX {
 		public static byte[] ValidatorV1PublicKey(HttpListenerRequest req) {
 			// TODO - VERIFY SERVICE TOKEN
 
-			// TODO - Is there a better way to do this? I'm new to c# 💀
+			// TODO - Is there a better way to do this? I'm new to C# 💀
 			byte[] publicKeyBytes = RSA_KEY_PAIR.ExportRSAPublicKey();
-			publicKeyBytes = rsaHeader.Concat(publicKeyBytes).ToArray(); //Add the starting ASN.1 that the game expects and C# doesn't generate...
+			publicKeyBytes = rsaHeader.Concat(publicKeyBytes).ToArray(); // * Add the starting ASN.1 that the game expects and C# doesn't generate...
+
 			string publicKeyBase64String = System.Convert.ToBase64String(publicKeyBytes);
 			byte[] publicKeyBase64Bytes = Encoding.ASCII.GetBytes(publicKeyBase64String);
 
-			byte[] responseData = new byte[EXPECTED_CERTIFICATE_ID.Length + publicKeyBase64Bytes.Length];
+			byte[] responseData = new byte[2 + publicKeyBase64Bytes.Length];
 
-			Array.Copy(EXPECTED_CERTIFICATE_ID, 0, responseData, 0, EXPECTED_CERTIFICATE_ID.Length);
-			Array.Copy(publicKeyBase64Bytes, 0, responseData, EXPECTED_CERTIFICATE_ID.Length, publicKeyBase64Bytes.Length);
+			BinaryPrimitives.WriteUInt16LittleEndian(responseData.AsSpan(), EXPECTED_CERTIFICATE_ID);
+			Array.Copy(publicKeyBase64Bytes, 0, responseData, 2, publicKeyBase64Bytes.Length);
 
 			return responseData;
 		}
@@ -199,30 +325,16 @@ namespace PKHaX {
 			string? customCertificateIDString = System.Environment.GetEnvironmentVariable("PKHAX_CERTIFICATE_ID");
 
 			if (!String.IsNullOrEmpty(customCertificateIDString)) {
-				int hexLength = customCertificateIDString.Length;
-
-				if (hexLength != 4) {
-					Console.WriteLine("Invalid certificate ID. Certificate IDs must be set as exactly 2 bytes represented as 4 hex characters (ex, ABCD), got {0}", customCertificateIDString);
-					System.Environment.Exit(1);
+				if (!UInt16.TryParse(customCertificateIDString, out ushort certificateID)) {
+					Console.WriteLine("Invalid certificate ID. Certificate IDs must be a number between 0 and 65535, got {0}", customCertificateIDString);
+					Environment.Exit(1);
 				}
 
-				byte[] bytes = new byte[hexLength / 2];
-				for (int i = 0; i < hexLength; i += 2) {
-					string chunk = customCertificateIDString.Substring(i, 2);
+				EXPECTED_CERTIFICATE_ID = certificateID;
 
-					try {
-						bytes[i / 2] = Convert.ToByte(chunk, 16);
-					} catch (System.Exception) {
-						Console.WriteLine("Invalid certificate ID. Chunk {0} is not valid hex. Certificate IDs must be set as exactly 2 bytes represented as 4 hex characters (ex, ABCD)", chunk);
-						System.Environment.Exit(1);
-					}
-				}
-
-				EXPECTED_CERTIFICATE_ID = bytes;
-
-				Console.WriteLine("Using certificate ID {0}", Convert.ToHexString(bytes));
+				Console.WriteLine("Using certificate ID {0}", EXPECTED_CERTIFICATE_ID);
 			} else {
-				Console.WriteLine("No certificate ID set. Using default ID {0}", Convert.ToHexString(EXPECTED_CERTIFICATE_ID));
+				Console.WriteLine("No certificate ID set. Using default ID {0}", EXPECTED_CERTIFICATE_ID);
 			}
 		}
 
